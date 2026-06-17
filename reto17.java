5f2f79cf869b8df896af309fe85c3f621a6aa7d6
jpablovegam-prog <jpablovegam@gmail.com>
2026-06-17 10:29:19 -0600

    Add files via upload

---
+++ reto17.java
@@ -0,0 +1,12 @@
+public class reto17 {
+    public static void main(String[]args) {
+        int n = 17;
+        int m = 6;
+
+        int c = n / m;
+        int r = n % m;
+
+        System.out.println(n + "entre" + m + "da un cociente" + c + "y un residuo" + r);
+    }
+
+}
