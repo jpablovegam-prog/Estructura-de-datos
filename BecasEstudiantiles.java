8dadcbdae047748ab9d7be3613e75af0a5f27836
jpablovegam-prog <jpablovegam@gmail.com>
2026-06-17 22:26:07 -0600

    Add files via upload

---
+++ BecasEstudiantiles.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class BecasEstudiantiles {
+    public static void main(String[] args) {
+
+        ArrayList<Double> calificaciones = new ArrayList<>();
+        ArrayList<Integer> asistencias = new ArrayList<>();
+        ArrayList<Boolean> concursos = new ArrayList<>();
+
+        // Calificaciones
+        calificaciones.add(95.5);
+        calificaciones.add(88.0);
+        calificaciones.add(72.5);
+        calificaciones.add(91.0);
+        calificaciones.add(65.0);
+        calificaciones.add(98.5);
+        calificaciones.add(84.0);
+        calificaciones.add(77.5);
+        calificaciones.add(93.0);
+        calificaciones.add(69.5);
+
+        // Asistencias
+        asistencias.add(95);
+        asistencias.add(85);
+        asistencias.add(70);
+        asistencias.add(82);
+        asistencias.add(90);
+        asistencias.add(100);
+        asistencias.add(78);
+        asistencias.add(88);
+        asistencias.add(92);
+        asistencias.add(65);
+
+        // Participación en concursos
+        concursos.add(false);
+        concursos.add(true);
+        concursos.add(false);
+        concursos.add(false);
+        concursos.add(true);
+        concursos.add(false);
+        concursos.add(false);
+        concursos.add(true);
+        concursos.add(false);
+        concursos.add(false);
+
+        // Evaluar beca
+        for (int i = 0; i < calificaciones.size(); i++) {
+
+            if ((calificaciones.get(i) >= 90 && asistencias.get(i) >= 80)
+                    || concursos.get(i)) {
+
+                System.out.println("Alumno " + (i + 1) + ": OBTIENE BECA");
+            } else {
+                System.out.println("Alumno " + (i + 1) + ": NO OBTIENE BECA");
+            }
+        }
+    }
+}
