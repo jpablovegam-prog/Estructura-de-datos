d8a3da79c24be18f1f88bfd4e0d4931c4da140bd
jpablovegam-prog <jpablovegam@gmail.com>
2026-07-09 09:59:12 -0600

    Add files via upload

---
+++ ListaEnlazada.java
@@ -0,0 +1,72 @@
+public class ListaEnlazada {
+    Nodo cabeza;
+
+    public void agregarNodoInicio(String datoNodo){
+        Nodo nuevo = new Nodo(datoNodo);
+        nuevo.siguienteNodo = cabeza;
+        cabeza = nuevo;
+    }
+
+    public void recorrido(){
+        Nodo actual = cabeza;
+        while (actual != null){
+            System.out.println(actual.dato);
+            actual = actual.siguienteNodo;
+        }
+    }
+
+    public void agregarFinal(String datoNodo){
+        Nodo nuevo = new Nodo(datoNodo);
+        if (cabeza == null){
+            cabeza = nuevo;
+            return;
+        }
+        Nodo actual = cabeza;
+        while(actual.siguienteNodo != null){
+            actual = actual.siguienteNodo;
+        }
+        actual.siguienteNodo = nuevo;
+    }
+
+    public boolean eliminarNodo(String datoEliminar){
+        if (cabeza == null){
+            return false;
+        }
+        Nodo actual = cabeza;
+        while (actual.siguienteNodo != null){
+            if(actual.siguienteNodo.dato.equals(datoEliminar)){
+                actual.siguienteNodo = actual.siguienteNodo.siguienteNodo;
+                return true;
+            }
+            actual = actual.siguienteNodo;
+        }
+        return false;
+    }
+
+    public boolean buscarNodo(String datoBuscar){
+        Nodo actual = cabeza;
+
+        while(actual != null){
+            if(actual.dato.equals(datoBuscar)){
+                return true;
+            }
+            actual = actual.siguienteNodo;
+        }
+
+        return false;
+    }
+
+    public boolean actualizarNodo(String datoAnterior, String datoNuevo){
+        Nodo actual = cabeza;
+
+        while(actual != null){
+            if(actual.dato.equals(datoAnterior)){
+                actual.dato = datoNuevo;
+                return true;
+            }
+            actual = actual.siguienteNodo;
+        }
+
+        return false;
+    }
+}
